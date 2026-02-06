80194f5eea0e9f97a5f4c6652bfadb9bf1a85af4
The-RuBe <rubenroldanjimenez12@gmail.com>
2026-02-06 11:03:26 +0100

    Editada la clase calculadora para añadir un método multiplicar.
    Además de añadir un SysOut en el main.

--- ActividadGit_RRJ_MDR/src/clasesGit/Calculadora.java
+++ ActividadGit_RRJ_MDR/src/clasesGit/Calculadora.java
@@ -1,12 +1,17 @@
 package clasesGit;
 
 public class Calculadora {
-	
+
 	public int sumar(int a, int b) {
 		return a + b;
 	}
+
 	public int restar(int a, int b) {
 		return a - b;
 	}
-	
+
+	public int multiplicar(int a, int b) {
+		return a * b;
+	}
+
 }
